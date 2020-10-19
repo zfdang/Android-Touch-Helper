8b7afcba8bdd1ce8d60bf93a61cdaa39f1525a64
dantifer <me@zfdang.com>
2020-10-19 21:55:17 +0800

    impl status fragment

--- app/src/main/java/com/zfdang/touchhelper/MainActivity.java
+++ app/src/main/java/com/zfdang/touchhelper/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
--- app/src/main/java/com/zfdang/touchhelper/ui/home/HomeFragment.java
+++ app/src/main/java/com/zfdang/touchhelper/ui/home/HomeFragment.java
@@ -1,11 +1,17 @@
 package com.zfdang.touchhelper.ui.home;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.PowerManager;
 import android.provider.Settings;
+import android.text.method.Touch;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,8 +31,11 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.zfdang.touchhelper.R;
+import com.zfdang.touchhelper.TouchHelperService;
 
 public class HomeFragment extends Fragment {
+
+    private final String TAG = getClass().getName();
 
     private HomeViewModel homeViewModel;
 
@@ -72,6 +81,18 @@ public class HomeFragment extends Fragment {
             }
         });
 
+        final ImageView imagePowerPermission = root.findViewById(R.id.image_power_permission);
+        homeViewModel.getPowerOptimization().observe(getViewLifecycleOwner(), new Observer<Boolean>() {
+            @Override
+            public void onChanged(Boolean aBoolean) {
+                if(aBoolean) {
+                    imagePowerPermission.setImageDrawable(drawableYes);
+                } else {
+                    imagePowerPermission.setImageDrawable(drawableNo);
+                }
+            }
+        });
+
 
         // set listener for buttons
         final Button btAppPermission = root.findViewById(R.id.button_app_permission);
@@ -97,11 +118,22 @@ public class HomeFragment extends Fragment {
                 Intent intent_abs = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
                 intent_abs.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 startActivity(intent_abs);
-                Toast.makeText(getContext(), "无障碍服务冲突，请关闭其中一个", Toast.LENGTH_SHORT).show();
+            }
+        });
 
-                MutableLiveData<Boolean> liveData = homeViewModel.getAccessibilityPermission();
-                boolean temp = (liveData.getValue() != null && liveData.getValue().booleanValue());
-                liveData.setValue(!temp);
+        final Button btPowerPermission = root.findViewById(R.id.button_power_permission);
+        btPowerPermission.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean hasIgnored = false;
+                PowerManager pm = (PowerManager) getContext().getSystemService(Context.POWER_SERVICE);
+
+                Intent intent = new Intent();
+                String packageName = getContext().getPackageName();
+                //  打开电池优化的界面，让用户设置
+                hasIgnored = pm.isIgnoringBatteryOptimizations(packageName);
+                intent.setAction(Settings.ACTION_IGNORE_BATTERY_OPTIMIZATION_SETTINGS);
+                startActivity(intent);
             }
         });
 
@@ -111,14 +143,28 @@ public class HomeFragment extends Fragment {
         return root;
     }
 
+    @Override
+    public void onResume() {
+        checkServiceStatus();
+        super.onResume();
+    }
+
     public void checkServiceStatus(){
 
-        // detect the app permission
-        boolean bAppPermission = false;
-
-        // set value to viewmodel
-        MutableLiveData<Boolean> liveData;
-        liveData = homeViewModel.getAppPermission();
+        // detect the app storage permission
+        boolean bAppPermission =
+                ContextCompat.checkSelfPermission(getContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
+        MutableLiveData<Boolean> liveData = homeViewModel.getAppPermission();
         liveData.setValue(bAppPermission);
+
+        // detect the accessibility permission
+        MutableLiveData<Boolean> accessibility = homeViewModel.getAccessibilityPermission();
+        accessibility.setValue(TouchHelperService.serviceImpl != null);
+
+        // detect power optimization
+        PowerManager pm = (PowerManager) getContext().getSystemService(Context.POWER_SERVICE);
+        boolean hasIgnored = pm.isIgnoringBatteryOptimizations(getContext().getPackageName());
+        MutableLiveData<Boolean> power = homeViewModel.getPowerOptimization();
+        power.setValue(hasIgnored);
     }
 }
